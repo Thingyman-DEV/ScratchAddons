842a54995dfcc95ce8011507ee5edfa72d41156f
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2020-09-26 09:07:31 -0400

    Update userscript.js

--- addons/more-whats-happening/userscript.js
+++ addons/more-whats-happening/userscript.js
@@ -1,5 +1,19 @@
 export default async function ({ addon, global, console }) {
-  var header = document.createElement('h1');
-header.textContent = "TTest!";
-document.body.appendChild(header);
+  const div = document.querySelector('box-header');
+  const link = document.createElement('a');
+ 
+                // Create the text node for anchor element. 
+                const link = document.createTextNode("This is link"); 
+                  
+                // Append the text node to anchor element. 
+                link.appendChild(link);  
+                  
+                // Set the title. 
+                link.title = "This is Link";  
+                  
+                // Set the href property. 
+                link.href = "https://scratch.mit.edu/messages/ajax/friends-activity/";  
+                  
+                // Append the anchor element to the body. 
+                document.body.appendChild(link);
 }
