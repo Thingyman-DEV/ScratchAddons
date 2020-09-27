519c0539d2e6a0a3c5252bbe67de0a5af9a7b419
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2020-09-27 16:57:32 -0400

    Update userscript_two.js

--- addons/more-whats-happening/userscript_two.js
+++ addons/more-whats-happening/userscript_two.js
@@ -1,5 +1,5 @@
 export default async function ({ addon, global, console }) {
 var top = document.getElementById('activity-feed');
-top.insertAdjacentHTML('afterbegin', '<h1>What's Happening<h1>');
+top.insertAdjacentHTML('afterbegin', '<h1>What's Happening</h1>');
 top.insertAdjacentHTML('beforebegin', '<a href:"https://scratch.mit.edu/">Go back to Scratch</a>');
 }
