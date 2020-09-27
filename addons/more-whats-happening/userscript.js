cf0831b4f8a6a91694e06f40c2743c6842d756d4
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2020-09-27 08:42:57 -0400

    Update userscript.js

--- addons/more-whats-happening/userscript.js
+++ addons/more-whats-happening/userscript.js
@@ -1,4 +1,5 @@
 export default async function ({ addon, global, console }) {
 var link = document.querySelector("p.box-header");
+console.log(link);
 link.insertAdjacentHTML('afterbegin', '<a href="https://scratch.mit.edu/messages/ajax/friends-activity/">See More</a>');
 }
