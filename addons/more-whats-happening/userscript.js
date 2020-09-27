6a5413bbafe52aae5ff7ccc5ffffeae71e77ed7e
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2020-09-27 08:28:07 -0400

    Maybe adds link

--- addons/more-whats-happening/userscript.js
+++ addons/more-whats-happening/userscript.js
@@ -1,19 +1,3 @@
 export default async function ({ addon, global, console }) {
-  const div = document.querySelector('box-header');
-  const link = document.createElement('a');
- 
-                // Create the text node for anchor element. 
-                const link = document.createTextNode("This is link"); 
-                  
-                // Append the text node to anchor element. 
-                link.appendChild(link);  
-                  
-                // Set the title. 
-                link.title = "This is Link";  
-                  
-                // Set the href property. 
-                link.href = "https://scratch.mit.edu/messages/ajax/friends-activity/";  
-                  
-                // Append the anchor element to the body. 
-                document.body.appendChild(link);
+document.querySelector(p.box-header).insertAdjacentHTML('afterbegin', '<a href="https://scratch.mit.edu/messages/ajax/friends-activity/">See More</a>')
 }
